7e8fe2510d5d34be9598066b62f221073a903c70
Dave Glick <dave@somedave.com>
2015-03-03 17:07:30 -0500

    Custom error pages now working (fetched via jquery ajax call)

--- ForkYeah/T4MVC.cs
+++ ForkYeah/T4MVC.cs
@@ -83,6 +83,8 @@ namespace Links
         private const string URLPATH = "~/Content";
         public static string Url() { return T4MVCHelpers.ProcessVirtualPath(URLPATH); }
         public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(URLPATH + "/" + fileName); }
+        public static readonly string _404_html = Url("404.html");
+        public static readonly string _500_html = Url("500.html");
         [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
         public static class bootstrap {
             private const string URLPATH = "~/Content/bootstrap";
